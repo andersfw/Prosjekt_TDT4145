592f0ed90dd4790cb9d83ea2cdacca2485547a94
andersfw <93136852+andersfw@users.noreply.github.com>
2023-03-18 14:53:11 +0100

    added functionality to historieH

--- historieH.py
+++ historieH.py
@@ -1,2 +1,31 @@
 # For en bruker skal man kunne finne all informasjon om de kjøpene hen har gjort for fremtidige
 # reiser. Denne funksjonaliteten skal programmeres.
+
+import sqlite3
+import re
+
+con = sqlite3.connect('tog2.db')
+
+cursor = con.cursor()
+
+def kjop_info(mobilnummer):
+    cursor.execute('''
+    SELECT * FROM KundeRegister 
+	JOIN KundeOrdre USING(KundeID) 
+	JOIN OrdrePaRute USING(OrdreNR) 
+	JOIN TogTur USING(TogruteID) 
+	JOIN Billett USING(OrdreNR)
+	
+	WHERE Mobilnummer = ?''', (mobilnummer))
+
+    return cursor.fetchall
+
+
+pattern = re.compile(r'^\d{8}$')
+mobilnummer = input('Angi telefon: ')
+
+while not bool(pattern.match(mobilnummer)): #Sjekker at telefonnummeret er gyldig
+    print('Ugyldig telefon')
+    mobilnummer = input('Angi telefon: ')
+
+print(kjop_info(mobilnummer))
