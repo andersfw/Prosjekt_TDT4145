7363efbe744fe7d14de96cf9b18913812a7ee97c
torjusostensen <torjusostensen@gmail.com>
2023-03-18 13:45:19 +0100

    endret dato-format på historieC.sql

--- historieC.sql
+++ historieC.sql
@@ -1,22 +1,22 @@
 -- Nordlandsbanen dag
-INSERT INTO TogTur VALUES ('03-04-2023', 1, 'Mandag');
-INSERT INTO TogTur VALUES ('04-04-2023', 1, 'Tirsdag');
-INSERT INTO TogTur VALUES ('05-04-2023', 1, 'Onsdag');
-INSERT INTO TogTur VALUES ('06-04-2023', 1, 'Torsdag');
-INSERT INTO TogTur VALUES ('07-04-2023', 1, 'Fredag');
+INSERT INTO TogTur VALUES ('2023-04-03', 1, 'Mandag');
+INSERT INTO TogTur VALUES ('2023-04-04', 1, 'Tirsdag');
+INSERT INTO TogTur VALUES ('2023-04-05', 1, 'Onsdag');
+INSERT INTO TogTur VALUES ('2023-04-06', 1, 'Torsdag');
+INSERT INTO TogTur VALUES ('2023-04-07', 1, 'Fredag');
 
 -- Nordlandsbanen natt
-INSERT INTO TogTur VALUES ('03-04-2023', 2, 'Mandag');
-INSERT INTO TogTur VALUES ('04-04-2023', 2, 'Tirsdag');
-INSERT INTO TogTur VALUES ('05-04-2023', 2, 'Onsdag');
-INSERT INTO TogTur VALUES ('06-04-2023', 2, 'Torsdag');
-INSERT INTO TogTur VALUES ('07-04-2023', 2, 'Fredag');
-INSERT INTO TogTur VALUES ('08-04-2023', 2, 'Lørdag');
-INSERT INTO TogTur VALUES ('09-04-2023', 2, 'Søndag');
+INSERT INTO TogTur VALUES ('2023-04-03', 2, 'Mandag');
+INSERT INTO TogTur VALUES ('2023-04-04', 2, 'Tirsdag');
+INSERT INTO TogTur VALUES ('2023-04-05', 2, 'Onsdag');
+INSERT INTO TogTur VALUES ('2023-04-06', 2, 'Torsdag');
+INSERT INTO TogTur VALUES ('2023-04-07', 2, 'Fredag');
+INSERT INTO TogTur VALUES ('2023-04-08', 2, 'Lørdag');
+INSERT INTO TogTur VALUES ('2023-04-09', 2, 'Søndag');
 
 -- Mobanen dag
-INSERT INTO TogTur VALUES ('03-04-2023', 3, 'Mandag');
-INSERT INTO TogTur VALUES ('04-04-2023', 3, 'Tirsdag');
-INSERT INTO TogTur VALUES ('05-04-2023', 3, 'Onsdag');
-INSERT INTO TogTur VALUES ('06-04-2023', 3, 'Torsdag');
-INSERT INTO TogTur VALUES ('07-04-2023', 3, 'Fredag');
+INSERT INTO TogTur VALUES ('2023-04-03', 3, 'Mandag');
+INSERT INTO TogTur VALUES ('2023-04-04', 3, 'Tirsdag');
+INSERT INTO TogTur VALUES ('2023-04-05', 3, 'Onsdag');
+INSERT INTO TogTur VALUES ('2023-04-06', 3, 'Torsdag');
+INSERT INTO TogTur VALUES ('2023-04-07', 3, 'Fredag');
