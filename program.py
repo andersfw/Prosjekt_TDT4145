e323aa330b1c9b47ad9d2081eb811985b07f6cad
andersfw <93136852+andersfw@users.noreply.github.com>
2023-03-21 11:05:50 +0100

    Program is working

--- program.py
+++ program.py
@@ -16,22 +16,34 @@ G - Finn og kjøp billetter
 H - Finn din reisehistorikk
 Q - Avslutte applikasjon\n''')
 
-gyldige_valg = ['c', 'd', 'e', 'g', 'h']
+gyldige_valg = ['c', 'd', 'e', 'g', 'h','q']
 valg = input('Hva vil du gjøre: (Skriv inn bokstav) ').lower()
 
-while valg not in gyldige_valg:
-    valg = input('Du må skrive inn en av bokstavene over: ').lower()
-
-while
-if valg == 'c':
-    historieC.main()
-elif valg == 'd':
-    historieD.main()
-elif valg == 'e':
-    historieE.main()
-elif valg == 'g':
-    historieG.main()
-elif valg == 'h':
-    historieH.main()
+while valg != 'q':
+    while valg not in gyldige_valg:
+        valg = input('Du må skrive inn en av bokstavene over: ').lower()
 
 
+    if valg == 'c':
+        historieC.main()
+    elif valg == 'd':
+        historieD.main()
+    elif valg == 'e':
+        historieE.main()
+    elif valg == 'g':
+        historieG.main()
+    elif valg == 'h':
+        historieH.main()
+    else:
+        break
+    valg = input('''Tjeneste utført! Hva vil du gjøre nå?\n
+C - Søk etter ruter langs en bestemt stasjon
+D - Finn alle avganger for to påfølgende dager mellom 2 stasjoner
+E - Registrer ny kunde
+G - Finn og kjøp billetter
+H - Finn din reisehistorikk
+Q - Avslutte applikasjon\n
+Velg her: ''').lower()
+
+print('\nTakk for at du brukte togdatabasen!\n')
+print('Restart programmet for å bruke det igjen.')
