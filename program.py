be0b66204c5ef97eca327f5472ff4e4c5850dfc3
andersfw <93136852+andersfw@users.noreply.github.com>
2023-03-22 09:22:38 +0100

    program

--- program.py
+++ program.py
@@ -8,13 +8,15 @@ print('\n!¤#Velkommen til togdatabasen!¤#')
 
 print('Dine valg i programmet er:')
 
-print('''
+alternativer = '''
 C - Søk etter ruter langs en bestemt stasjon
 D - Finn alle avganger for to påfølgende dager mellom 2 stasjoner
 E - Registrer ny kunde
 G - Finn og kjøp billetter
 H - Finn din reisehistorikk\n
-Q - Avslutte applikasjon\n''')
+Q - Avslutte applikasjon\n'''
+
+print(alternativer)
 
 gyldige_valg = ['c', 'd', 'e', 'g', 'h','q']
 valg = input('Hva vil du gjøre: (Skriv inn bokstav) ').lower()
@@ -36,14 +38,7 @@ while valg != 'q':
         historieH.main()
     else:
         break
-    valg = input('''\nTjeneste utført! Hva vil du gjøre nå?\n
-C - Søk etter ruter langs en bestemt stasjon
-D - Finn alle avganger for to påfølgende dager mellom 2 stasjoner
-E - Registrer ny kunde
-G - Finn og kjøp billetter
-H - Finn din reisehistorikk\n
-Q - Avslutte applikasjon\n
-Velg her: ''').lower()
+    valg = input(f'\nTjeneste utført! Hva vil du gjøre nå?\n' +alternativer + '\nVelg her: ').lower()
 
 print('\nTakk for at du brukte togdatabasen!\n')
 print('Restart programmet (pil opp) for å bruke det igjen.')
