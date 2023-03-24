e35c367eb4a18b584c0a21da137cf2537cd3a5c6
Kristoffer Wetterhus <kkw@dhcp-10-22-15-130.wlan.ntnu.no>
2023-03-24 12:43:00 +0100

    Kommentarer på G

--- brukerhistorier/historieG.py
+++ brukerhistorier/historieG.py
@@ -11,11 +11,14 @@ import time
 
 
 def main(db):
+
+    # Kobler til databasen 'db' som er satt i 'program.py'.
     con = sqlite3.connect(db)
 
     cursor = con.cursor()
 
-    def hentPlasseringer(TogruteID):  # Henter ut sete-/sengeplasser fra togruten
+    # Henter ut alle mulige sete- og sengeplasser fra togruten.
+    def hentPlasseringer(TogruteID):
         cursor.execute('''SELECT VognOppsett.TogVognNR, Sete.SeteNR, Seng.SengNR, VognOppsett.VognID
             FROM Togrute
             INNER JOIN VognOppsett ON Togrute.TogruteID = VognOppsett.TogruteID
@@ -26,7 +29,8 @@ def main(db):
 
         return cursor.fetchall()
 
-    def hentBilletter(TogruteID, Dato):  # Henter ut alle billettene knyttet til en togtur
+    # Henter ut alle billettene knyttet til en togtur
+    def hentBilletter(TogruteID, Dato):
         cursor.execute('''SELECT VognOppsett.TogVognNR, Billett.SeteNR, Billett.SengNR, OrdrePaRute.TogruteID, Billett.StartStasjon, Billett.SluttStasjon
             FROM Billett JOIN OrdrePaRute ON Billett.OrdreNR = OrdrePaRute.OrdreNR
             JOIN (Togrute
@@ -41,6 +45,7 @@ def main(db):
 
         return cursor.fetchall()
 
+    # Henter ut alle stasjonene på en togrute. Brukes kun i 'sjekkOmMellomStasjoner()'.
     def hentStasjoner(TogruteID):
         cursor.execute('''SELECT PaDelstrekning.TogruteID, PaDelstrekning.DelstrekningID, Delstrekning.DelstrekningID, 
             Delstrekning.StartStasjon, Delstrekning.SluttStasjon
@@ -52,6 +57,8 @@ def main(db):
         stasjoner.append(stash[-1][-1])
         return stasjoner
 
+    # Sjekker om det er overlapp mellom to strekninger.
+    # Dette er for å sjekke hvilke deler av en togrute en billett gjelder på i 'hentLedigePlasser()'
     def sjekkOmMellomStasjoner(startStasjonNy, sluttStasjonNy, startStasjonGammel, sluttStasjonGammel, TogruteID):
         alle_stasjoner = hentStasjoner(TogruteID)
         stasjoner_ny = []
@@ -72,6 +79,10 @@ def main(db):
 
         return fellesStasjon(stasjoner_ny, stasjoner_gammel)
 
+    # Brukes som en hjelpefunksjon i sjekkOmMellomStasjoner.
+    # Sjekker om de to listene med stasjoner har noen til felles.
+    # Det går fint å ha én felles stasjon da én billett kan være til en stasjon,
+    # og så er en annen billett videre fra den samme stasjonen.
     def fellesStasjon(ny, gammel):
         result = 0
         for x in ny:
@@ -81,7 +92,8 @@ def main(db):
                     result += 1
         return result > 1
 
-    # Legger inn en ny bestilling KUN DEMO-VERDIER NÅ
+    # Legger inn en KundeOrdre i databasen, samtidig som den oppdaterer OrdrePaRute,
+    # som kun gjøres én gang per runde en kunde bestiller billetter.
     def leggInnOrdre(OrdreNR, DatoBestilling, KlBestilling, KundeID, DatoTur, TogruteID):
 
         cursor.execute('''INSERT INTO KundeOrdre
@@ -94,7 +106,8 @@ def main(db):
 
         con.commit()
 
-    # Legger inn en ny bestilling KUN DEMO-VERDIER NÅ
+    # Legger inn en ny billett i databasen. Dette skjer for hver plass på togruten som bestilles,
+    # altså flere ganger per kundeordre dersom det er bestilt flere antall plasser.
     def leggInnBestilling(OrdreNR, StartStasjon, SluttStasjon, SeteNR, SengNR, VognID):
 
         cursor.execute('''INSERT INTO Billett
@@ -102,35 +115,52 @@ def main(db):
 
         con.commit()
 
-    # Henter antall ledige sete-/sengeplasser på en togtur
+    # Henter antall ledige sete- og sengeplasser på en togtur.
+    # Dette gjøres ved å hente alle mulige plasser på en togrute vha. hentPlasseringer(),
+    # for så å fjerne plasser dersom det eksisterer en billett på samme togtur, vogn og plass.
     def hentLedigePlasser(TogruteID, Dato, StartStasjonNy, SluttStasjonNy):
         ledigePlasseringer = hentPlasseringer(TogruteID)
         billetter = hentBilletter(TogruteID, Dato)
         ledige_senger = []
         ledige_seter = []
+
+        # Legger til alle sitteplasser i én liste, og alle sengeplasser i en annen,
+        # for å enklere kunne håndtere forskjellene i egenskapene til de to plass-typene.
         for p in ledigePlasseringer:
             if p[1] != None:
                 ledige_seter.append(p)
             elif p[2] != None:
                 ledige_senger.append(p)
-        for e in billetter:
-            if e[1] != None:  # hvis billetten og plasseringen har samme TogVognNR
+
+        for b in billetter:
+            # Hvis billetten er en sete-billett
+            if b[1] != None:
                 for sete in ledige_seter:
-                    # Sjekker om det er overlapp mellom stasjoner på billetten og det som bestilles, hvis ikke beholder den plasseringen
-                    if e[1] == sete[1] and e[0] == sete[0] and sjekkOmMellomStasjoner(StartStasjonNy, SluttStasjonNy, e[4], e[5], TogruteID):
+                    # Sjekker først om seteNR på billetten og på plassen er det samme.
+                    # Sjekker så at vognNR på billetten og på plassen er det samme.
+                    # Sjekker til slutt om det er overlapp mellom stasjoner på billetten og det som bestilles, hvis ikke beholder den plasseringen.
+                    if b[1] == sete[1] and b[0] == sete[0] and sjekkOmMellomStasjoner(StartStasjonNy, SluttStasjonNy, b[4], b[5], TogruteID):
+                        # Hvis if-setningen er sann, vil setet bli fjernet fra ledige_seter, som da tilsier at det blir fjernet fra ledige_plasseringer.
                         ledige_seter.remove(sete)
-            elif e[2] != None:
+            # Hvis billetten er en seng-billett
+            elif b[2] != None:
                 for seng in ledige_senger:
-                    if e[2] == seng[2] and e[0] == seng[0]:
+                    # Sjekker først om sengNR på billetten og på plassen er det samme.
+                    # Sjekker så at vognNR på billetten og på plassen er det samme.
+                    if b[2] == seng[2] and b[0] == seng[0]:
                         next_index = ledige_senger.index(seng)
+                        # Hvis if-setningen er sann, vil sengen bli fjernet fra ledige_senger, som da tilsier at det blir fjernet fra ledige_plasseringer.
                         ledige_senger.remove(seng)
-                        if e[2] in [1, 3, 5, 7]:
+                        # Fjerner neste seng dersom den er i samme kupé som sengen i billetten.
+                        if b[2] in [1, 3, 5, 7]:
                             ledige_senger.pop(next_index)
 
+        # Legger sammen ledige seter og senger til en felles oversikt over alle ledige plasser.
         ledigePlasseringer = []
         ledigePlasseringer.extend(ledige_seter)
         ledigePlasseringer.extend(ledige_senger)
 
+        # Finner antallet ledige seter og senger.
         ledig = [0, 0]
         for x in ledigePlasseringer:
             if x[1] != None:
@@ -139,6 +169,8 @@ def main(db):
                 ledig[1] += 1
 
         return ledig, ledigePlasseringer
+
+    ### Ferdig kommentert hit ###
 
     # Skriver ut de mulig avgangene i et oversiktlig format
     def printResultater(res, start, slutt):
@@ -185,6 +217,8 @@ def main(db):
             f'Da har du bestilt: {res[tur][11]} fra {start} til {slutt} kl. {res[tur][6]}, {res[tur][14]} {res[tur][13]}.')
         print('Gå til billett-oversikten for å se billettene dine.')
         print('\n')
+
+    ### PROGRAM / INPUT ###
 
     cursor.execute('SELECT Navn FROM Jernbanestasjon;')
     gyldige_stasjoner = [row[0] for row in cursor.fetchall()]
@@ -315,5 +349,3 @@ def main(db):
     con.close()
 
     time.sleep(2)
-
-# Er alle sovevogner alltid 8 senger/4 kupeer?
